e23e628bd29d4485fda254d77268248ae7c35b69
Thosine-01 <lawaltosine01@gmail.com>
2025-01-28 13:46:01 +0100

    PayBills Done

--- src/app/payBills/page.jsx
+++ src/app/payBills/page.jsx
@@ -56,9 +56,7 @@ const PayBills = () => {
           </div>
           <AgentKycComponent />
               <TransactionsTable />
-              
-
-              xs
+     
         </>
       );
   }
