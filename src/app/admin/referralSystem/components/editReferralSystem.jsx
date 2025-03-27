74642e3f65ab555692c7a3cc79b32abd36eaeda3
emdevelopa <gyimahsunday@gmail.com>
2025-03-27 17:20:36 +0100

    Referraal System

---
+++ src/app/admin/referralSystem/components/editReferralSystem.jsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState } from "react";
+import {
+  FaPlus,
+  FaTrashAlt,
+  FaUserAlt,
+  FaUserCog,
+  FaUsers,
+  FaUserTag,
+} from "react-icons/fa";
+
+const EditReferralSystem = () => {
+  const [formData, setFormData] = useState({
+    username: "Chosenfolio",
+    password: "Chosenfolio",
+    userType: "Reseller",
+    status: "Approved",
+    accountNo: "Chosenfolio",
+    permissions: {
+      active: false,
+      admin: false,
+      superUser: false,
+    },
+  });
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      permissions: {
+        ...prev.permissions,
+        [name]: checked,
+      },
+    }));
+  };
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">{formData.username}</h2>
+      <div className="flex bg-gray-50">
+        <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-[90%]">
+          <form className="space-y-4 flex flex-col gap-8">
+            {/*User Info*/}
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-[6em]">
+                <label className="block text-[18px] font-medium text-gray-700 ">
+                  <input type="checkbox" className="mr-[2em]" name="" id="" />
+                  Refferal username
+                </label>
+                <div className="flex flex-col gap-1">
+                  <input
+                    type="text"
+                    value={formData.username}
+                    className="w-full mt-1 p-4 border rounded-[0.8em] focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    disabled
+                  />
+                  <p className="text-[14px] text-gray-500">
+                    Requires 50 characters or fewer, digits and @#?/+- only
+                  </p>
+                </div>
+                <p>$10</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-[6em]">
+                <label className="block text-[18px] font-medium text-gray-700 ">
+                  <input type="checkbox" className="mr-[2em]" name="" id="" />
+                  Refferal username
+                </label>
+                <div className="flex flex-col gap-1">
+                  <input
+                    type="text"
+                    value={formData.username}
+                    className="w-full mt-1 p-4 border rounded-[0.8em] focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    disabled
+                  />
+                  <p className="text-[14px] text-gray-500">
+                    Requires 50 characters or fewer, digits and @#?/+- only
+                  </p>
+                </div>
+                <p>$700</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-[6em]">
+                <label className="block text-[18px] font-medium text-gray-700 ">
+                  <input type="checkbox" className="mr-[2em]" name="" id="" />
+                  Refferal username
+                </label>
+                <div className="flex flex-col gap-1">
+                  <input
+                    type="text"
+                    value={formData.username}
+                    className="w-full mt-1 p-4 border rounded-[0.8em] focus:outline-none focus:ring-2 focus:ring-gray-400"
+                    disabled
+                  />
+                  <p className="text-[14px] text-gray-500">
+                    Requires 50 characters or fewer, digits and @#?/+- only
+                  </p>
+                </div>
+                <p>$60</p>
+              </div>
+            </div>
+
+            <div className="flex gap-3">
+              {/* <button className="bg-[#00613A] font-medium text-white px-4 py-2 rounded-lg flex items-center gap-2">
+                Save <FaPlus />
+              </button> */}
+              <button className="bg-[#8C1823] font-medium text-white px-4 py-2 rounded-lg flex items-center gap-2">
+                Delete <FaTrashAlt />
+              </button>{" "}
+              {/* <button className="bg-[#282EFF] font-medium text-white px-4 py-2 rounded-lg flex items-center gap-2">
+                Impersonate <FaUsers />
+              </button> */}
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditReferralSystem;
---
+++ src/app/admin/transactionManagement/transactionManagement.jsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import TrxManagementTable from "./components/TrxManagementTable";
+import Pagination from "../components/pagination";
+import TableTabs from "../components/tableTabs";
+import EditUserTrx from "./components/editUserTransaction";
+import { FaChevronRight } from "react-icons/fa";
+
+const TransactionManagement = () => {
+  const [activeTabPending, setActiveTabPending] =
+    React.useState("All Transaction");
+  const data = [
+    {
+      id: 1,
+      user: "John Doe",
+      product: "Airtime",
+      amount: "50,000",
+      date: "2024-03-01",
+      status: "Completed",
+    },
+    {
+      id: 2,
+      user: "Jane Smith",
+      product: "Internet",
+      amount: "10,000",
+      date: "2024-03-02",
+      status: "Pending",
+    },
+    {
+      id: 3,
+      user: "Alice Johnson",
+      product: "Transfer",
+      amount: "20,000",
+      date: "2024-03-03",
+      status: "Failed",
+    },
+    {
+      id: 4,
+      user: "Bob Brown",
+      product: "Cable",
+      amount: "15,000",
+      date: "2024-03-04",
+      status: "Refunded",
+    },
+    {
+      id: 5,
+      user: "Charlie Davis",
+      product: "Electricity",
+      amount: "30,000",
+      date: "2024-03-05",
+      status: "Completed",
+    },
+  ];
+
+  const editTrx = true;
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  return (
+    <div className="overflow-hidden ">
+      <div className="max-md-[400px]:hidden">
+        {editTrx ? (
+          <>
+            <div>
+              <h1 className="text-[16px] font-semibold mb-8 flex items-center gap-4">
+                Transaction Management <FaChevronRight /> Edit User Transaction
+              </h1>
+            </div>
+            <EditUserTrx />
+          </>
+        ) : (
+          <>
+            <h1 className="text-[16px] font-semibold mb-8">
+              Transaction Management
+            </h1>
+
+            <TableTabs
+              header={""}
+              setActiveTab={setActiveTabPending}
+              activeTab={activeTabPending}
+              tabs={["All Transaction", "Inactive", "Recently Added"]}
+              from="transactionManagement"
+            />
+            <div className="rounded-t-[1em] overflow-auto border border-gray-200 min-h-[50vh]">
+              <TrxManagementTable
+                data={data}
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+              />
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={setCurrentPage}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TransactionManagement;
