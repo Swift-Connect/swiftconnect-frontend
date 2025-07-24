f2bfb4fe017ad6677be64a0cde188ac6d5ba27f9
emdevelopa <gyimahsunday@gmail.com>
2025-07-24 22:09:32 +0100

    updates

--- src/app/Agent/page.jsx
+++ src/app/Agent/page.jsx
@@ -52,7 +52,7 @@ export default function Agent() {
             </p>
           </div>
 
-          <div className="flex flex-col gap-4 w-full md:w-auto">
+          <div className="flex flex-col gap-4 w-full md:w-auto">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
             <div className="flex items-center gap-4">
               <Image
                 src={"./money_sack.svg"}
