c0b84b7ab6a19ba6b281f732c848d1e9b2583c95
emdevelopa <gyimahsunday@gmail.com>
2025-08-04 19:29:26 +0100

    added the edit button on transactyion management

--- src/app/admin/reportsAndAnalytics/page.jsx
+++ src/app/admin/reportsAndAnalytics/page.jsx
@@ -1,15 +1,36 @@
-"use client"
+"use client";
+import api from "@/utils/api";
 import DashboardOverview from "./components/dashboardOverview";
 import LastSection from "./components/lastSectiion";
 import SalesReport from "./components/salesReport";
 import ServiceRevenue from "./components/servicesRevenue";
+import { use, useEffect } from "react";
 export default function ReporstAndAnalytics() {
+  // const fetchAllPages = async (endpoint) => {
+  //   let allData = [];
+
+  //   try {
+  //     const res = await api.get(endpoint);
+  //     // allData = allData.concat(res.data.results || res.data);
+  //     console.log(res.data);
+      
+  //     // nextPage = res.data.next || null;
+  //   } catch (error) {
+  //     // toast.error(`Error fetching data from /api/analytics/`);
+  //     console.error(`Error fetching /api/analytics/:`, error);
+  //   }
+
+  //   return allData;
+  // };
+  // useEffect(() => {
+  //   fetchAllPages("https://swiftconnect-backend.onrender.com/api/analytics/");
+  // }, []);
   return (
-      <main className="min-h-screen bg-[#F5FBF7] ">
-          <DashboardOverview />
-          <SalesReport />
-          <ServiceRevenue />
-          <LastSection />
+    <main className="min-h-screen bg-[#F5FBF7] ">
+      <DashboardOverview />
+      <SalesReport />
+      <ServiceRevenue />
+      <LastSection />
     </main>
   );
 }
